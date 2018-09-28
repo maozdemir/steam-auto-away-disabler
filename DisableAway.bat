b60e6c11873b5cbe808ba0920b4e99880077ce34
impulsiva <alperozdemir@mail.com>
2018-09-28 03:38:52 +0300

    Update DisableAway.bat

--- DisableAway.bat
+++ DisableAway.bat
@@ -1,7 +1,7 @@
 @echo off
 :loop
 start steam://friends/status/online
-echo Status set to Online at %time%
+echo Status Away was defused at %time%
 echo Waiting for 5 minutes for next update.
 timeout /t 300>null
 goto loop
